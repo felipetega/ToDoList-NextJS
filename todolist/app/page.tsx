03c68bdad5d0e6cccc36e7a919555f39a40fe060
felipetega <felipe.crema.ribeiro@gmail.com>
2023-05-17 17:30:20 -0300

    initial setup

---
+++ todolist/app/page.tsx
@@ -0,0 +1,14 @@
+import AddTask from "./components/AddTask";
+import ToDoList from "./components/ToDoList";
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">ToDoList App</h1>
+        <AddTask />
+      </div>
+      <ToDoList />
+    </main>
+  )
+}
