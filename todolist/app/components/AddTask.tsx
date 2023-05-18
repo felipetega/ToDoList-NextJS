65b7ed6b56c99874c97c628dbcc260c93d529bd6
felipetega <felipe.crema.ribeiro@gmail.com>
2023-05-18 16:00:37 -0300

    add tasks

--- todolist/app/components/AddTask.tsx
+++ todolist/app/components/AddTask.tsx
@@ -3,14 +3,24 @@
 import { AiFillPlusCircle } from 'react-icons/ai';
 import Modal from './Modal';
 import { FormEventHandler, useState } from "react";
+import { addTodo } from '@/api';
+import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
+  const router = useRouter()
   const [modalOpen, setModalOpen] = useState<boolean>(false)
   const [newTaskValue, setNewTaskValue] = useState<string>('');
 
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue
+    })
     setNewTaskValue("")
+    setModalOpen(false)
+    router.refresh()
   }
 
   return (
--- todolist/app/components/Task.tsx
+++ todolist/app/components/Task.tsx
@@ -1,15 +1,50 @@
+"use client";
+
 import { ITask } from "@/types/task"
+import { FiEdit, FiTrash2 } from 'react-icons/fi'
+import { useState, FormEventHandler } from "react";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+//import { v4 as uuidv4 } from "uuid";
 
 interface TaskProps {
     task: ITask
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+    const router = useRouter()
+    const [openModalEdit, setOpenModalEdit] = useState<boolean>(false)
+    const [openModalDelete, setOpenModalDelete] = useState<boolean>(false)
+    const [taskToEdit, setTaskToEdit] = useState<string>(task.text)
+
+    const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        // await addTodo({
+        //   id: uuidv4(),
+        //   text: newTaskValue
+        // })
+        setTaskToEdit("")
+        setOpenModalEdit(false)
+        router.refresh()
+      }
+
   return (
     <>
         <tr key={task.id}>
-            <td>{task.text}</td>
-            <td>Blue</td>
+            <td className="w-full">{task.text}</td>
+            <td className="flex gap-5">
+                <FiEdit onClick={() => setOpenModalEdit(true)} cursor="pointer" className="text-blue-500" size={25} />
+                <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+                    <form onSubmit={handleSubmitEditTodo}>
+                        <h3 className='font-bold text-lg'>Edit task</h3>
+                        <div className='modal-action'>
+                            <input onChange={e => setTaskToEdit(e.target.value)} value={taskToEdit} type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                            <button type='submit' className='btn'>Submit</button>
+                        </div>
+                    </form>
+                </Modal>
+                <FiTrash2 cursor="pointer" className="text-red-500" size={25}/>
+            </td>
         </tr>
     </>
   )
